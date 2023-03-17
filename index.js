e5d7c74fef0a72ef7d611370bfdf0c19bc0f986a
Hein Htet <90121395+JamesCartar@users.noreply.github.com>
2023-03-17 18:34:49 +0630

    chainging the import file path

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 
-const connectDB = require('./config/connectDb.js')
+const connectDB = require('./config/connectDB.js')
 const { notFound } = require("./milddleware/notFound.js")
 const { errorHandler } = require("./milddleware/errorHandler.js")
 const { authMiddleware } = require("./milddleware/auth.js")
